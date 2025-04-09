6ce39d602eed81c9c98f83230f4553123d0d093d
Deni0012 <deni.dg00@icloud.com>
2025-04-09 15:33:11 +0200

    feat: created list

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,29 @@
 import { useState } from 'react'
 import './App.css'
 
+const listSalmo = [
+  {
+    id: 1,
+    title: "Playlist"
+  },
+  {
+    id: 2,
+    title: "Ranch"
+  },
+  {
+    id: 3,
+    title: "Flop"
+  },
+  {
+    id: 4,
+    title: "Hellvisback"
+  },
+  {
+    id: 5,
+    title: "Midnite"
+  },
+]
+
 function App() {
   const [] = useState(0)
 
